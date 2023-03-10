da38f9c2400f961f64bdaf3a8ae27e1e10e2d31a
dzming-git <dzm_work@163.com>
2023-03-10 11:06:26 +0800

    兼容DBot_SDK0.2版本，添加自动下载功能

--- app/services/service.py
+++ app/services/service.py
@@ -2,13 +2,19 @@ import imapclient
 import os
 import email
 import chardet
+import threading
+import time
 from email.header import decode_header
+from DBot_SDK import send_message
 from conf.experiment_info import ExperimentInfo
 from utils import file_name_handler
 
 class EMailServer:
     _email_server = None
+    _email_address = ''
     _messages = []
+    _auto_download_thread = None
+    _auto_download_stop = True
 
     @classmethod
     def connect_email(cls, gid=None, qid=None, msg_list=[]):
@@ -17,30 +23,22 @@ class EMailServer:
             message_parts.append(f'[CQ:at,qq={qid}]')
         # 获取邮箱信息
         email_info = ExperimentInfo.get_email_info()
-        email_address = email_info.get('address', None)
+        cls._email_address = email_info.get('address', None)
         email_password = email_info.get('password', None)
         server_host = email_info.get('host', None)
         # 连接到IMAP服务器
         cls._email_server = imapclient.IMAPClient(server_host, ssl= True)
-        cls._email_server.login(email_address, email_password)
+        cls._email_server.login(cls._email_address, email_password)
         cls._email_server.id_({"name": "IMAPClient", "version": imapclient.version.version})
         info = cls._email_server.select_folder('INBOX', readonly = True)
-        # 搜索所有未读邮件
-        cls._messages = cls._email_server.search()
-        message_parts.append(f'连接成功，共{len(cls._messages)}条邮件')
-        # processed_email_ids = ExperimentInfo.get_processed_email_ids()
         
-        message_send = '\n'.join(message_parts).rstrip('\n')
-        return message_send
+        send_message(f'登录{cls._email_address}成功', gid, qid)
     
     @classmethod
-    def download_attachment(cls, gid=None, qid=None, msg_list=[]):
-        message_parts = []
-        if gid:
-            message_parts.append(f'[CQ:at,qq={qid}]')
-        processed_email_ids = ExperimentInfo.get_processed_email_ids()
-        unprocessed_email_ids = [id for id in cls._messages if id not in processed_email_ids]
-        for id, message_data in cls._email_server.fetch(unprocessed_email_ids, 'RFC822').items():
+    def download_attachment_by_email_ids(cls, email_ids, gid, qid):
+        if type(email_ids) is int:
+            email_ids = [email_ids]
+        for uid, message_data in cls._email_server.fetch(email_ids, 'RFC822').items():
             email_message = email.message_from_bytes(message_data[b'RFC822'])
             for part in email_message.walk():
                 # 如果是附件
@@ -55,15 +53,49 @@ class EMailServer:
                             folder_path = os.path.join(ExperimentInfo.get_save_path(), time_info, experiment_id)
                             file_path = os.path.join(folder_path, decoded_filename)
                             os.makedirs(folder_path, exist_ok=True)
-                            message_parts.append(f'下载{decoded_filename}')
+                            send_message(f'{decoded_filename}\n下载成功', gid, qid)
                             # 下载附件
                             with open(file_path, 'wb') as f:
                                 f.write(part.get_payload(decode=True))
                                 f.close()
-        ExperimentInfo.note_processed_email_ids(unprocessed_email_ids)
-        message_send = '\n'.join(message_parts).rstrip('\n')
-        return message_send
+                            response = cls._email_server.fetch(uid, ['FLAGS'])
+                            flags = response[uid][b'FLAGS']
+                            if b'\\Seen' not in flags:
+                                cls._email_server.set_flags(uid, [b'\\Seen'])
 
+    @classmethod
+    def download_attachment(cls, gid=None, qid=None, msg_list=[]):
+        # 搜索所有未读邮件
+        try:
+            cls._messages = cls._email_server.search()
+        except:
+            send_message(f'{cls._email_address}已断开连接，正在重连', gid, qid)
+            cls.connect_email(gid, qid, msg_list)
+            return cls.download_attachment(gid, qid, msg_list)
+        processed_email_ids = ExperimentInfo.get_processed_email_ids()
+        unprocessed_email_ids = [id for id in cls._messages if id not in processed_email_ids]
+        cls.download_attachment_by_email_ids(unprocessed_email_ids, gid, qid)
+        ExperimentInfo.note_processed_email_ids(unprocessed_email_ids)
+
+    @classmethod
+    def auto_download_attachment(cls, gid=None, qid=None, msg_list=[]):
+        flag = 'start'
+        if msg_list:
+            flag = msg_list[0]
+        if flag == 'start' or flag == '开始':
+            def auto_download_attachment_thread(gid, qid):
+                while not cls._auto_download_stop:
+                    cls.download_attachment(gid, qid)
+                    time.sleep(30)
+                # 用户终止程序时，退出 IDLE 模式
+                cls._email_server.idle_done()
+                send_message('自动下载已退出', gid, qid)
+            cls._auto_download_thread = threading.Thread(target=auto_download_attachment_thread, args=(gid, qid,), name='auto_download')
+            cls._auto_download_stop = False
+            cls._auto_download_thread.start()
+        elif flag == 'stop' or flag == '停止':
+            cls._auto_download_stop = True
+        
 func_dict = {
     '#连接邮箱': {
         'func': EMailServer.connect_email,
@@ -72,5 +104,9 @@ func_dict = {
     '#下载附件':{
         'func': EMailServer.download_attachment,
         'permission': 'ROOT'
-        }
+        },
+    '#自动下载':{
+        'func': EMailServer.auto_download_attachment,
+        'permission': 'ROOT'
+        },
     }
